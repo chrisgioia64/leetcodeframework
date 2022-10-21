68fe3c74170a94916e731af9c9f8a613b1595c4a
Christopher Gioia <christopher.g@brillio.com>
2022-10-21 16:24:50 -0700

    forgot to include AlgorithmResult

---
+++ src/main/java/base/result/AlgorithmResult.java
@@ -0,0 +1,36 @@
+package base.result;
+
+import base.Algorithm;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+public abstract class AlgorithmResult<X, Y> {
+
+    @Getter
+    protected final List<TestResult<X, Y>> results;
+
+    @Getter
+    protected final Algorithm<X, Y> simpleAlgorithm;
+
+    @Getter
+    protected int numCorrect;
+
+    protected AlgorithmResult(List<TestResult<X, Y>> results, Algorithm<X, Y> simpleAlgorithm) {
+        this.results = results;
+        this.simpleAlgorithm = simpleAlgorithm;
+        computeStatistics();
+    }
+
+    protected void computeStatistics() {
+        int numCorrect = 0;
+        for (TestResult result : results) {
+            if (result.isSimilar()) {
+                numCorrect++;
+            }
+        }
+    }
+
+
+}
